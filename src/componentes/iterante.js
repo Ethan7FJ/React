0f1601217312daaec1d987195d0a8c3c59f6f235
natalia <johanfetecua11@gmail.com>
2024-06-28 13:14:15 -0500

    28/06/2024

--- src/componentes/iterante.js
+++ src/componentes/iterante.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Card from "./Card";
 
-const Iterador = ({chao})=>(
-    <React.Fragment>
-        {
-            chao.map((chao)=>{
-                    <Card
-                        key={chao.id_b}
-                        {...chao}
-                    />
-            })
-        }
-    </React.Fragment>
-)
+const Iterador = ({chao})=>{
+    return(
+        <React.Fragment>
+            {chao ? (
+                chao.length > 0 ?(
+                    chao.map((chao) => (
+                        <Card
+                            key={chao.id_b}
+                            {...chao}
+                        />
+                    ))
+                ) : (
+                    <p> No hay datos disponibles</p>
+            )
 
+        ) : (
+            <p>Cargando...</p>
+           )}
+
+        </React.Fragment>
+    );
+};
 export default Iterador
